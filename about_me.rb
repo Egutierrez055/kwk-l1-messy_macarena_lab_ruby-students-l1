d53495dcff104f268a1c1ebf5b1e2df4752f073b
Egutierrez055 <egutierrez055@gmail.com>
2018-06-05 21:13:51 +0000

    automatically backed up by learn

---
+++ about_me.rb
@@ -0,0 +1,9 @@
+def about me 
+  puts "My name is Elizabeth Gutierrez"
+  puts " I have a son named Miguel"
+  puts "I am the eldest of seven"
+  puts "I love Riverdale"
+  puts "I will be attending the University of Denver"
+  puts "I graduated from South High School May 31"
+  puts "I love Colorado and my parents home town"
+end
